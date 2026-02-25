73ebb47da1e5294f058d4939fcc980d3e5d522b8
where where <qq2518766683@outlook.com>
2026-02-25 14:14:56 +0800

    修正百度统计

--- SharpScript/Site/helpers/analytics.ts
+++ SharpScript/Site/helpers/analytics.ts
@@ -11,30 +11,26 @@ declare global {
 }
 
 export function useAnalytics() {
-    useHead({
-        meta: [
-            { name: "google-site-verification", content: "TPDbPzhpaqYCFwTabtkmyHbwnULZEHrAihJNMsmpsks" },
-            { name: "msvalidate.01", content: "C979E9108FB600BD7D5A68A70678F7AD" },
-            { name: "yandex-verification", content: "e15f6a5a8ad325cd" },
-        ]
-    });
-
     window.dataLayer = window.dataLayer || [];
     function gtag(key: string, value: any): void;
     function gtag() { window.dataLayer.push(arguments); }
     window.gtag = gtag;
     gtag("js", new Date());
     gtag("config", "G-6FDQ56LCDG");
-    useScript({
-        src: "https://www.googletagmanager.com/gtag/js?id=G-6FDQ56LCDG",
-        async: true
+
+    window._hmt = window._hmt || [];
+
+    useHead({
+        meta: [
+            { name: "google-site-verification", content: "TPDbPzhpaqYCFwTabtkmyHbwnULZEHrAihJNMsmpsks" },
+            { name: "msvalidate.01", content: "C979E9108FB600BD7D5A68A70678F7AD" },
+            { name: "yandex-verification", content: "e15f6a5a8ad325cd" },
+        ],
+        script: [
+            { src: "https://www.googletagmanager.com/gtag/js?id=G-6FDQ56LCDG", async: true },
+            { src: "https://hm.baidu.com/hm.js?271f5d3abe652ce0122880579c66289e", async: true }
+        ]
     });
 
     injectScript("m7ua7korsz");
-
-    window._hmt = window._hmt || [];
-    useScript({
-        src: "https://hm.baidu.com/hm.js?271f5d3abe652ce0122880579c66289e",
-        async: true
-    });
 }
